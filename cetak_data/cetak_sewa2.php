30c3870302a3b5af294d18bc4f8c88607066a9d3
yusufbagussh <yusuf.herlambang27@gmail.com>
2022-07-18 18:51:35 +0700

    Perbaikan nama kolom

--- cetak_data/cetak_sewa2.php
+++ cetak_data/cetak_sewa2.php
@@ -40,7 +40,7 @@ foreach ($rows as $row) {
     $html .= '<tr>
                 <td>' . $i++ . '</td>
                 <td>' . $row["nama_member"] . '</td>
-                <td>' . $row["id_member"] . '</td>
+                <td>' . $row["id_penyewa"] . '</td>
                 <td>' . $row["nama_barang"] . '</td>
                 <td>' . $row["id_barang"] . '</td>
                 <td>' . $row["harga_sewa"] . '</td>
